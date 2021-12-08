8a679303fcb2dbb5e2435defc87f7fee6347a660
wangpradit01 <hwangpradisth@gmail.com>
2021-12-09 01:32:05 +0700

    commit

--- lib/search.dart
+++ lib/search.dart
@@ -10,6 +10,7 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   TextEditingController? _textEditingController = TextEditingController();
+  List<String> foodListOnSearch = [];
   List<String> foodList = [
     'ก๋วยเตี๋ยว',
     'ก๋วยเตี๋ยวคั่วไก่',
@@ -26,6 +27,55 @@ class _SearchState extends State<Search> {
     'แกงมัสมั่น',
     'แกงเลียง',
     'แกงส้ม',
+    'ขนมลา',
+    'ข้าวแกง',
+    'ข้าวคลุกกะปิ',
+    'ข้าวแช่',
+    'ข้าวต้ม',
+    'ข้าวตังหน้าตั้ง',
+    'ข้าวผัด',
+    'ข้าวผัดอเมริกัน',
+    'ข้าวมันไก่',
+    'ข้าวมันส้มตำ',
+    'ข้าวสวย',
+    'ข้าวเสียโป',
+    'ข้าวหน้าเป็ด',
+    'ไข่น้ำ',
+    'ค้างคาวเผือก',
+    'โจ๊ก',
+    'ชาไทย',
+    'ซอสศรีราชา',
+    'ต้มจิ๋ว',
+    'ต้มยำ',
+    'ต้มยำกุ้ง',
+    'ต้มส้ม',
+    'น้ำเงี้ยว',
+    'น้ำจิ้ม',
+    'น้ำพริก',
+    'น้ำพริกกะปิ',
+    'น้ำพริกลงเรือ',
+    'ใบเมี่ยง',
+    'ผัดกระเพรา',
+    'ผัดขี้เมา',
+    'ผัดซีอิ้ว',
+    'ผัดไทย',
+    'พริกกะเกลือ',
+    'พะโล้',
+    'แม่โขง',
+    'ราดหน้า',
+    'ลาบ',
+    'ส้มตำ',
+    'สะเต๊ะ',
+    'สัพแหยก',
+    'หมก',
+    'หมูแดง',
+    'หอยทอด',
+    'อาจาด',
+    // '',
+    // '',
+    // '',
+    // '',
+    // '',
   ];
   @override
   Widget build(BuildContext context) {
@@ -36,7 +86,12 @@ class _SearchState extends State<Search> {
                 color: Colors.blue.shade300,
                 borderRadius: BorderRadius.circular(30)),
             child: TextField(
-              onChanged: (value){},
+              onChanged: (value){
+                setState(() {
+                  foodListOnSearch = foodList.where((element) => element.contains(value)).toList();
+                });
+                
+              },
               controller: _textEditingController,
               decoration: InputDecoration(
                   border: InputBorder.none,
@@ -51,7 +106,7 @@ class _SearchState extends State<Search> {
         drawer: MainDraw(),
         body: ListView.builder(
           
-          itemCount: foodList.length,
+          itemCount: _textEditingController!.text.isNotEmpty? foodListOnSearch.length: foodList.length,
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.all(8.0),
@@ -61,7 +116,8 @@ class _SearchState extends State<Search> {
                     child: Icon(Icons.food_bank),
                   ),
                   SizedBox(width: 10),
-                  Text(foodList[index],
+                  Text(
+                    _textEditingController!.text.isNotEmpty? foodListOnSearch[index]: foodList[index],
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
